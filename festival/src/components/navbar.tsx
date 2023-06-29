3a7e79ca53cef3efc1103a2aa391aa6ff398b0b8
Julia Santana <santana.j@ufba.br>
2023-06-29 18:15:34 -0300

    add desktok navbar

---
+++ festival/src/components/navbar.tsx
@@ -0,0 +1,41 @@
+import styles from './navbar.module.css';
+import Link from 'next/link';
+
+
+function Navbar(){
+    return (
+        <nav className={styles.Navbar}>
+          <ul>
+          <li className={styles.logo}>
+                東京都
+            </li>
+          <li>
+              <Link href="/">
+                Início
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                 Atrações
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                Ingressos
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                Cadastro
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                Login
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      );
+}
+export default Navbar;
