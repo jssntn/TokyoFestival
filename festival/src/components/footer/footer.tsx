580da0130dd12af478edc9221ec6061b89371e3d
Julia Santana <santana.j@ufba.br>
2023-07-08 20:28:20 -0300

    style updates

--- festival/src/components/footer/footer.tsx
+++ festival/src/components/footer/footer.tsx
@@ -8,50 +8,50 @@ function Footer(){
     <>
     <footer className={styles.footer}>
         <div className={styles.content}>
-        <div className={styles.footerContainer}>
-            <h3>Festival</h3>
-            <ul>
-                <li>
-                    <a href="#">Parceiros</a>
-                </li>
-                <li>
-                    <a href="#">Imprensa</a>
-                </li>
-                <li>
-                    <a href="#">Sobre nós</a>
-                </li>
-            </ul>
-        </div>
+            <div className={styles.footerContainer}>
+                <h3>Festival</h3>
+                <ul>
+                    <li>
+                        <a href="#">Parceiros</a>
+                    </li>
+                    <li>
+                        <a href="#">Imprensa</a>
+                    </li>
+                    <li>
+                        <a href="#">Sobre nós</a>
+                    </li>
+                </ul>
+            </div>
 
-        <div className={styles.footerContainer}>
-            <h3>Ajuda</h3>
-            <ul>
-                <li>
-                    <a href="#">Contato</a>
-                </li>
-                <li>
-                    <a href="#">Informações</a>
-                </li>
-            </ul>
-        </div>
+            <div className={styles.footerContainer}>
+                <h3>Ajuda</h3>
+                <ul>
+                    <li>
+                        <a href="#">Contato</a>
+                    </li>
+                    <li>
+                        <a href="#">Informações</a>
+                    </li>
+                </ul>
+            </div>
 
-        <div className={styles.footerContainer}>
-            <h3>Social</h3>
-            <ul>
-                <li>
-                    <a href="#">Instagram</a>
-                </li>
-                <li>
-                    <a href="#">Facebook</a>
-                </li>
-                <li>
-                    <a href="#">Twitter</a>
-                </li>
-                <li>
-                    <a href="#">Tiktok</a>
-                </li>
-            </ul>
-        </div>
+            <div className={styles.footerContainer}>
+                <h3>Social</h3>
+                <ul>
+                    <li>
+                        <a href="#">Instagram</a>
+                    </li>
+                    <li>
+                        <a href="#">Facebook</a>
+                    </li>
+                    <li>
+                        <a href="#">Twitter</a>
+                    </li>
+                    <li>
+                        <a href="#">Tiktok</a>
+                    </li>
+                </ul>
+            </div>
         </div>
         <hr/>
        <div className={styles.footerBottom}>
